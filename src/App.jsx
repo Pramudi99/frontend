6d67c3f92f8245e5662c44c67c3e0ca9c611dc86
chinthaka senarathna <chironprabath@gmail.com>
2024-12-01 01:18:36 +0530

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import {  Routes, Route, BrowserRouter } from 'react-router-dom';
 import Search from './Components/Search'
+import Test from './Components/Test';
 
 
 export default function App() {
   return (
     <BrowserRouter>  
-   <Routes> 
-    <Route path="/search" element={<Search/>} />    
-   </Routes> 
+    <Routes> 
+      <Route path="/search" element={<Search/>}/>
+      <Route path='/test' element={<Test/>}/>    
+    </Routes> 
    </BrowserRouter>   
   )
 }

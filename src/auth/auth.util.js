cf2208e743e3f7d328a707467d03914c8de562d3
chinthaka senarathna <chironprabath@gmail.com>
2024-12-01 12:55:40 +0530

    set the session

---
+++ src/auth/auth.util.js
@@ -0,0 +1,12 @@
+import axiosInstance from "../util/axiosInstance";
+
+export const setSession = (accessToken) => {
+    if(accessToken){
+        localStorage.setItem("accessToken",accessToken);
+        axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+    }
+    else{
+        localStorage.removeItem("accessToken");
+        delete axiosInstance.defaults.headers.common.Authorization;
+    }
+};

baed0cc6587698f93f7d287b2e7d4a144a2aeb16
Chathuminipamodya <ies.22.chathumini@gmail.com>
2024-12-02 10:36:12 +0530

    seller page changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {  Routes, Route, BrowserRouter } from 'react-router-dom';
 import Search from './Components/Search'
+import SellerLandingPage from './SellerLandingPage';
 
 
 export default function App() {
   return (
     <BrowserRouter>  
    <Routes> 
-    <Route path="/search" element={<Search/>} />    
+    <Route path="/search" element={<Search/>} />   
+    <Route path="/sellerlandingpage" element={<SellerLandingPage />} /> 
    </Routes> 
    </BrowserRouter>   
   )

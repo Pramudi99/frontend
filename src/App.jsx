eb22a02da39775110b09862642344370f854d7ab
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-12-03 11:48:26 +0530

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {  Routes, Route, BrowserRouter } from 'react-router-dom';
 import Search from './Components/Search'
 import Item from './Components/Item';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 
 export default function App() {
@@ -10,6 +12,8 @@ export default function App() {
    <Routes> 
     <Route path="/search" element={<Search/>} />   
     <Route path="/item/:id" element={<Item/>} /> 
+    <Route path="/login" element={<LoginPage/>} /> 
+    <Route path="/register" element={<RegisterPage/>} />
    </Routes> 
    </BrowserRouter>   
   )

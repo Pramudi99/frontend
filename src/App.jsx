1c38fedbe039bdee45dfef6a361ac47a772b836f
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-12-03 11:56:07 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import {  Routes, Route, BrowserRouter } from 'react-router-dom';
 import Search from './Components/Search'
-
-import SellerLandingPage from './SellerLandingPage';
-
+import SellerLandingPage from './Components/SellerLandingPage';
 import Item from './Components/Item';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
+import Sidebar from './Components/Sidebar';
 
 
 
@@ -15,10 +14,13 @@ export default function App() {
     <BrowserRouter>  
    <Routes> 
     <Route path="/search" element={<Search/>} />   
-
-    <Route path="/sellerlandingpage" element={<SellerLandingPage />} /> 
-
     <Route path="/item/:id" element={<Item/>} /> 
+
+    <Route path="/login" element={<LoginPage/>} /> 
+    <Route path="/register" element={<RegisterPage/>} />
+
+    <Route path="/dashboard" element={<Sidebar/>} /> 
+    <Route path="/seller-landing-page" element={<SellerLandingPage/>} />
 
    </Routes> 
    </BrowserRouter>   
